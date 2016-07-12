53e56a62e120907344d12b528c4bd74596f9c601
Icaro Soares <icarosoares01@gmail.com>
2016-07-11 22:15:31 -0300

    Create models objects

--- server/models.py
+++ server/models.py
@@ -1,5 +1,24 @@
 from __future__ import unicode_literals
 
+from django.utils.encoding import python_2_unicode_compatible
 from django.db import models
 
 # Create your models here.
+
+
+@python_2_unicode_compatible
+class Provider(models.Model):
+
+    name = models.CharField(max_length=256)
+
+    def __str__(self):
+        return self.name
+
+
+@python_2_unicode_compatible
+class OperationSystem(models.Model):
+
+    name = models.CharField(max_length=256)
+
+    def __str__(self):
+        return self.name
--- server/tests.py
+++ server/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
 
+from models import Provider, OperationSystem
+
 # Create your tests here.
+
+
+class ServerTestCase(TestCase):
+
+    def setUp(self):
+        """start test object"""
+        Provider.objects.create(name='Amazon')
+        OperationSystem.objects.create(name='Windows')
+
+    def teste_get_object(self):
+
+        provider = Provider.objects.get(name='Amazon')
+        system = OperationSystem.objects.get(name='Windows')
+        self.assertEqual(provider.name, 'Amazon')
+        self.assertEqual(system.name, 'Windows')
